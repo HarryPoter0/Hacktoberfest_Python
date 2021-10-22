f1cdf0b7e1d66cfba32a8e2126a999d825a8ce71
Harsh Mandaliya <32090768+hashmand@users.noreply.github.com>
2021-10-22 20:29:59 +0530

    Update set_union.py

--- Python/set_union.py
+++ Python/set_union.py
@@ -1,15 +1,3 @@
-# farm_animals = {"sheep", "hen",  "cow", "horse", "goat"}
-# wild_animals =  {"lion", "tiger", "goat", "panther"}
-#
-# all = farm_animals.union(wild_animals)
-# print(all)
-#
-# all2 = wild_animals.union(farm_animals)
-# print(all2)
-#
-# all3 = wild_animals | farm_animals
-# print(all3)
-
 from prescription_data import adverse_interactions
 
 meds_to_watch = set()
