9e3a9e4f39f9d16cad9ec2e6013fc08b79856c2e
Harsh Mandaliya <32090768+hashmand@users.noreply.github.com>
2023-10-29 16:08:44 +0530

    Matplotlib Line
    Added matplotlib_dotted_line.py

---
+++ Python/matplotlib_dotted_line.py
@@ -0,0 +1,14 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
