b196e5a029dbf25d26f151f0d3ec96499d2a04cb
Harsh Mandaliya <32090768+hashmand@users.noreply.github.com>
2023-10-29 15:41:34 +0530

    Update matplotlib_marker.py

--- Python/matplotlib_marker.py
+++ Python/matplotlib_marker.py
@@ -1,1 +1,15 @@
-v
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+lt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
